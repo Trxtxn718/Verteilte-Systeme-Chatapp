acba9434cabd4c1815fbf397947ba2780b3ca489
Tristan Kopp <Tristan.Kopp@dhbw-engineering.de>
2024-10-24 01:08:33 +0200

    Update database connection to use Sequelize ORM

--- server/server.js
+++ server/server.js
@@ -1,46 +1,7 @@
 'use strict';
 
 const express = require('express');
-
-// Database
-const mysql = require('mysql');
-// Database connection info - used from environment variables
-var dbInfo = {
-    connectionLimit : 10,
-    host: process.env.MYSQL_HOSTNAME,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-};
-
-var connection = mysql.createPool(dbInfo);
-console.log("Conecting to database...");
-// connection.connect(); <- connect not required in connection pool
-
-// SQL Database init.
-// In this current demo, this is done by the "database.sql" file which is stored in the "db"-container (./db/).
-// Alternative you could use the mariadb basic sample and do the following steps here:
-/*
-connection.query("CREATE TABLE IF NOT EXISTS table1 (task_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, description TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)  ENGINE=INNODB;", function (error, results, fields) {
-    if (error) throw error;
-    console.log('Answer: ', results);
-});
-*/
-// See readme.md for more information about that.
-
-// Check the connection
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error; // <- this will throw the error and exit normally
-    // check the solution - should be 2
-    if (results[0].solution == 2) {
-        // everything is fine with the database
-        console.log("Database connected and works");
-    } else {
-        // connection is not fine - please check
-        console.error("There is something wrong with your database connection! Please check");
-        process.exit(5); // <- exit application with error code e.g. 5
-    }
-});
+const { Sequelize} = require('sequelize');
 
 
 // Constants
@@ -49,6 +10,23 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Database connection
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOSTNAME,
+    dialect: 'mariadb'
+});
+
+// Test the database connection
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 
 // Features for JSON Body
 app.use(express.urlencoded({ extended: true }));

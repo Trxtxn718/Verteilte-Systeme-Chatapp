13032fa749ea28f1a24ea37d5c6ab99b08a7f3ab
Trxtxn718 <tristankopp@gmx.net>
2024-10-23 15:37:03 +0200

    removed old routes

--- server/server.js
+++ server/server.js
@@ -56,83 +56,12 @@ app.use(express.json());
 
 // Entrypoint - call it with: http://localhost:8080/ -> redirect you to http://localhost:8080/static
 app.get('/', (req, res) => {
-    console.log("Got a request and redirect it to the static page");
-    // redirect will send the client to another path / route. In this case to the static route.
-    res.redirect('/static');
+
+    res.status(200)
 });
-
-// Another GET Path - call it with: http://localhost:8080/special_path
-app.get('/special_path', (req, res) => {
-    res.send('This is another path');
-});
-
-// Another GET Path that shows the actual Request (req) Headers - call it with: http://localhost:8080/request_info
-app.get('/request_info', (req, res) => {
-    console.log("Request content:", req)
-    res.send('This is all I got from the request:' + JSON.stringify(req.headers));
-});
-
-// POST Path - call it with: POST http://localhost:8080/client_post
-app.post('/client_post', (req, res) => {
-    if (typeof req.body !== "undefined" && typeof req.body.post_content !== "undefined") {
-        var post_content = req.body.post_content;
-        console.log("Client send 'post_content' with content:", post_content)
-        // Set HTTP Status -> 200 is okay -> and send message
-        res.status(200).json({ message: 'I got your message: ' + post_content });
-    }
-    else {
-        // There is no body and post_contend
-        console.error("Client send no 'post_content'")
-        // Set HTTP Status -> 400 is client error -> and send message
-        res.status(400).json({ message: 'This function requries a body with "post_content"' });
-    }
-});
-
-// ###################### BUTTON EXAMPLE ######################
-// POST path for Button 1
-app.post('/button1_name', (req, res) => {
-    // Load the name from the formular. This is the ID of the input:
-    const name = req.body.name
-    // Print it out in console:
-    console.log("Client send the following name: " + name + " | Button1")
-    // Send JSON message back - this could be also HTML instead.
-    res.status(200).json({ message: 'I got your message - Name is: ' + name });
-    // More information here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms
-})
-
-// GET path for Button 2
-app.get('/button2', (req, res) => {
-    // This will generate a random number and send it back:
-    const random_number = Math.random();
-    // Print it out in console:
-    console.log("Send the following random number to the client: " + random_number + " | Button2")
-    // Send it to the client / webbrowser:
-    res.send("Antwort: " + random_number);
-    // Instead of plain TXT - the answer could be a JSON
-    // More information here: https://www.w3schools.com/xml/ajax_intro.asp
-});
-// ###################### BUTTON EXAMPLE END ######################
-
 
 // ###################### DATABASE PART ######################
-// GET path for database
-app.get('/database', (req, res) => {
-    console.log("Request to load all entries from table1");
-    // Prepare the get query
-    connection.query("SELECT * FROM `table1`;", function (error, results, fields) {
-        if (error) {
-            // we got an errror - inform the client
-            console.error(error); // <- log error in server
-            res.status(500).json(error); // <- send to client
-        } else {
-            // we got no error - send it to the client
-            console.log('Success answer from DB: ', results); // <- log results in console
-            // INFO: Here could be some code to modify the result
-            res.status(200).json(results); // <- send it to client
-        }
-    });
-});
-app.get('/database2', (req, res) => {
+app.get('/users', (req, res) => {
     console.log("Request to load all entries from table1");
     // Prepare the get query
     connection.query("SELECT * FROM `Users`;", function (error, results, fields) {
@@ -148,60 +77,8 @@ app.get('/database2', (req, res) => {
         }
     });
 });
-// DELETE path for database
-app.delete('/database/:id', (req, res) => {
-    // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
-    let id = req.params.id; // <- load the ID from the path
-    console.log("Request to delete Item: " + id); // <- log for debugging
 
-    // Actual executing the query to delete it from the server
-    // Please keep in mind to secure this for SQL injection!
-    connection.query("DELETE FROM `table1` WHERE `table1`.`task_id` = " + id + ";", function (error, results, fields) {
-        if (error) {
-            // we got an errror - inform the client
-            console.error(error); // <- log error in server
-            res.status(500).json(error); // <- send to client
-        } else {
-            // Everything is fine with the query
-            console.log('Success answer: ', results); // <- log results in console
-            // INFO: Here can be some checks of modification of the result
-            res.status(200).json(results); // <- send it to client
-        }
-    });
-});
 
-// POST path for database
-app.post('/database', (req, res) => {
-    // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
-    // it will be added to the database with a query.
-    if (typeof req.body !== "undefined" && typeof req.body.title !== "undefined" && typeof req.body.description !== "undefined") {
-        // The content looks good, so move on
-        // Get the content to local variables:
-        var title = req.body.title;
-        var description = req.body.description;
-        console.log("Client send database insert request with 'title': " + title + " ; description: " + description); // <- log to server
-        // Actual executing the query. Please keep in mind that this is for learning and education.
-        // In real production environment, this has to be secure for SQL injection!
-        connection.query("INSERT INTO `table1` (`task_id`, `title`, `description`, `created_at`) VALUES (NULL, '" + title + "', '" + description + "', current_timestamp());", function (error, results, fields) {
-            if (error) {
-                // we got an errror - inform the client
-                console.error(error); // <- log error in server
-                res.status(500).json(error); // <- send to client
-            } else {
-                // Everything is fine with the query
-                console.log('Success answer: ', results); // <- log results in console
-                // INFO: Here can be some checks of modification of the result
-                res.status(200).json(results); // <- send it to client
-            }
-        });
-    }
-    else {
-        // There is nobody with a title nor description
-        console.error("Client send no correct data!")
-        // Set HTTP Status -> 400 is client error -> and send message
-        res.status(400).json({ message: 'This function requries a body with "title" and "description' });
-    }
-});
 // ###################### DATABASE PART END ######################
 
 
@@ -219,12 +96,3 @@ console.log(`Running on http://${HOST}:${PORT}`);
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
-
-
-
-
-
-
-
-
-

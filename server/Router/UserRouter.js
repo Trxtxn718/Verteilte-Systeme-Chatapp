5fabd3a4a953d3bdd085a1a95e96981ce9a3866c
Trxtxn718 <tristankopp@gmx.net>
2024-10-26 00:49:44 +0200

    Added all routes

--- server/Router/UserRouter.js
+++ server/Router/UserRouter.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const { Users } = require('../models/Users');
 
 router.get('/', (req, res) => {
-    console.log("Request to load all entries from table1");
-    // Prepare the get query
     Users.findAll().then(users => {
         res.status(200).json(users);
     }).catch(err => {
@@ -13,5 +11,37 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/:id', (req, res) => {
+    Users.findByPk(req.params.id).then(user => {
+        res.status(200).json(user);
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    Users.create(req.body).then(user => {
+        res.status(201).json(user);
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Users.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        res.status(204).send();
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json(err);
+    });
+}
+);
 
 module.exports = router;

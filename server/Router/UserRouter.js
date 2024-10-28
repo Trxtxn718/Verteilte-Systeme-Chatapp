4862a8b4d01738415d3843046d567cdaf0f8f9d7
Quax2013 <robin.alex.ernst@gmail.com>
2024-10-28 14:27:48 +0100

    Added: Include JSON Web Tokens

--- server/Router/UserRouter.js
+++ server/Router/UserRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const { Users } = require('../models/Users');
 
+// Get all users DEBUG
+// TODO delete
 router.get('/', (req, res) => {
     Users.findAll().then(users => {
         res.status(200).json(users);
@@ -12,6 +15,7 @@ router.get('/', (req, res) => {
     });
 });
 
+// Get user information of user with id
 router.get('/:id', (req, res) => {
     Users.findByPk(req.params.id).then(user => {
         res.status(200).json(user);
@@ -21,13 +25,38 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+// Try to create new user with attributes
+router.post('/register', async (req, res) => {
+    let user = await Users.findByPk(req.body.id)
+    if (user != null) {
+        res.status(409).json({
+            message: "User already exists."
+        })
+        return;
+    }
+    if (req.body.email == null || req.body.password == null || req.body.username == null){
+        res.status(400).json({
+            message: "User data not complete."
+        });
+        return;
+    }
+
+    req.body.created = new Date().toISOString();
+    req.body.updated = req.body.created;
+    if (req.body.profile_img == null) {
+        req.body.profile_img = "test-img.png";
+    }
     Users.create(req.body).then(user => {
+        user.token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
         res.status(201).json(user);
     }).catch(err => {
         console.error(err);
         res.status(500).json(err);
     });
+});
+
+router.post('/login', (req, res) => {
+
 });
 
 router.delete('/:id', (req, res) => {

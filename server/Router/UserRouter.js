4427c4b8ec738cfd6042916366ba6bf5c5d31d22
Quax2013 <robin.alex.ernst@gmail.com>
2024-10-30 13:35:09 +0100

    Added: Login functionality; Added: Comments

--- server/Router/UserRouter.js
+++ server/Router/UserRouter.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 
 const { Users } = require('../models/Users');
 
-// Get all users DEBUG
+// Try to get all users !!!DEBUG!!!
 // TODO delete
 router.get('/', (req, res) => {
     Users.findAll().then(users => {
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
     });
 });
 
-// Get user information of user with id
+// Try to get user information of user with id
 router.get('/:id', (req, res) => {
     Users.findByPk(req.params.id).then(user => {
         res.status(200).json(user);
@@ -28,6 +28,15 @@ router.get('/:id', (req, res) => {
 
 // Try to create new user with attributes
 router.post('/register', async (req, res) => {
+    // Check if user data is complete
+    if (req.body.email == null || req.body.password == null || req.body.username == null) {
+        res.status(400).json({
+            message: "User data not complete."
+        });
+        return;
+    }
+
+    // Check if user already exists
     let user = await Users.findOne({ where: { email: req.body.email } });
     console.log(user);
     if (user != null) {
@@ -36,20 +45,17 @@ router.post('/register', async (req, res) => {
         })
         return;
     }
-    if (req.body.email == null || req.body.password == null || req.body.username == null) {
-        res.status(400).json({
-            message: "User data not complete."
-        });
-        return;
-    }
 
-    req.body.password = crypto.pbkdf2Sync(req.body.password, process.env.PASSWORD_SALT, 1000, 64, 'sha512').toString('hex'); // TODO VERIFY FUNCTIONALITY
+    // Add password hash and created date
+    req.body.password = crypto.pbkdf2Sync(req.body.password, process.env.PASSWORD_SALT, 1000, 64, 'sha512').toString('hex');
     req.body.created = new Date().toISOString();
     req.body.updated = req.body.created;
     if (req.body.profile_img == null) {
         req.body.profile_img = "test-img.png";
     }
+    // Create user
     Users.create(req.body).then(user => {
+        // Set cookie with token
         res.cookie('token', jwt.sign({ id: user.id }, process.env.JWT_SECRET), {
             httpOnly: true,
             secure: true,
@@ -65,10 +71,48 @@ router.post('/register', async (req, res) => {
     });
 });
 
+// Try to login user with email and password
 router.post('/login', (req, res) => {
+    // Check if user data is complete
+    if (req.body.email == null || req.body.password == null) {
+        res.status(400).json({
+            message: "User data not complete."
+        });
+        return;
+    }
 
+    // Check if user exists
+    Users.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user == null) {
+            res.status(401).json({
+                message: "User not found."
+            });
+            return;
+        }
+        // Check if password is correct
+        if (user.password != crypto.pbkdf2Sync(req.body.password, process.env.PASSWORD_SALT, 1000, 64, 'sha512').toString('hex')) {
+            res.status(401).json({
+                message: "Password incorrect."
+            });
+            return;
+        }
+        // Set cookie with token
+        res.cookie('token', jwt.sign({ id: user.id }, process.env.JWT_SECRET), {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        });
+        res.status(200).json({
+            message: "User logged in."
+        });
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json(err);
+    });
 });
 
+// Try to delete user with id
 router.delete('/:id', (req, res) => {
     Users.destroy({
         where: {

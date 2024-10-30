6030270cc94e3361323e4372c4455be8c89f389f
Quax2013 <robin.alex.ernst@gmail.com>
2024-10-30 09:40:03 +0100

    Added: Register functionality with hashed password;

--- server/Router/UserRouter.js
+++ server/Router/UserRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const { Users } = require('../models/Users');
 
@@ -27,28 +28,37 @@ router.get('/:id', (req, res) => {
 
 // Try to create new user with attributes
 router.post('/register', async (req, res) => {
-    let user = await Users.findByPk(req.body.id)
+    let user = await Users.findOne({ where: { email: req.body.email } });
+    console.log(user);
     if (user != null) {
         res.status(409).json({
             message: "User already exists."
         })
         return;
     }
-    if (req.body.email == null || req.body.password == null || req.body.username == null){
+    if (req.body.email == null || req.body.password == null || req.body.username == null) {
         res.status(400).json({
             message: "User data not complete."
         });
         return;
     }
 
+    req.body.password = crypto.pbkdf2Sync(req.body.password, process.env.PASSWORD_SALT, 1000, 64, 'sha512').toString('hex'); // TODO VERIFY FUNCTIONALITY
     req.body.created = new Date().toISOString();
     req.body.updated = req.body.created;
     if (req.body.profile_img == null) {
         req.body.profile_img = "test-img.png";
     }
     Users.create(req.body).then(user => {
-        user.token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-        res.status(201).json(user);
+        res.cookie('token', jwt.sign({ id: user.id }, process.env.JWT_SECRET), {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        });
+        res.status(201).json({
+            message: "User created."
+        });
     }).catch(err => {
         console.error(err);
         res.status(500).json(err);

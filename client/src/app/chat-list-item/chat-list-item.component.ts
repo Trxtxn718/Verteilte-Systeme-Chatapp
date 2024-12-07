c64f194fcead0c2f90e7f1839bd0d7ad9af42b31
Quax2013 <robin.alex.ernst@gmail.com>
2024-12-07 14:21:16 +0100

    Added: Styling to chat Added: Arguments to chat list item

--- client/src/app/chat-list-item/chat-list-item.component.ts
+++ client/src/app/chat-list-item/chat-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-list-item',
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './chat-list-item.component.scss'
 })
 export class ChatListItemComponent {
+  @Input() name?: string;
+  @Input() lastMessage?: string;
+  @Input() lastMessageTime?: string;
 
+  constructor() {
+    if (!this.name) {
+      this.name = '';
+    }
+    if (!this.lastMessage) {
+      this.lastMessage = '';
+    }
+    if (!this.lastMessageTime) {
+      this.lastMessageTime = '0.0.0, 00:00';
+    }
+
+    console.log('ChatListItemComponent created');
+  }
+
+  clicked() {
+    console.log('ChatListItemComponent clicked');
+  }
 }
